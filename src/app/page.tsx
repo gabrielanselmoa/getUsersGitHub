52d658d7bd0bfdaa8526b11291608ca895a60342
Gabriel <gabrielanselmo29@gmail.com>
2023-10-09 10:44:46 -0300

    keyEvent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,55 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, KeyboardEvent } from "react";
 import { json } from "stream/consumers";
+import { Validation } from "@/components/validation";
 
 const Page = () => {
 
   const [data, setData] = useState<string>("");
   const [users, setUsers] = useState<any>();
+  const [message, setMessage] = useState<any>(null)
+  const [clicked, setClicked] = useState<boolean>(false)
 
   const getInputData = (e: React.ChangeEvent<HTMLInputElement>) => {
     setData(e.target.value)
   }
   
   const handleFetch = async () => {
-    const url = fetch(`https://api.github.com/users/${data}`)
-    const res = await url
-    const json = await res.json()
 
-    console.log(json);
+    try {
+      const url = fetch(`https://api.github.com/users/${data}`)
+      const res = await url
+      const json = await res.json()
 
-    setUsers(json)
+      // console.log(json);
+      setMessage(json.status);
+      setUsers(json)
+
+    } catch (message) {
+      
+    }
+
   }
-  
+
+  const handleGetUsers = () => {
+    setClicked(true)
+    handleFetch()
+  }
+
+  const handleKeyDown = (e:KeyboardEvent) => {
+    if(e.key === "Enter"){
+      handleGetUsers()
+    }
+  }
+
   return (
     <main>
 
       {/* SEARCH SECTION */}
       <section className="search-section">
         <div className="search-container">
-          <h1 className="title">GitHub User Search</h1>
+          <h1 className="title">GitHub User Finder</h1>
 
           <h4>Search for a User:</h4>
           <p>See the user profile information</p>
@@ -39,38 +60,42 @@ const Page = () => {
             placeholder="username here"
             value={data}
             onChange={getInputData}
+            onKeyDown={handleKeyDown}
             />
-            <button className="btn" onClick={handleFetch}>Search</button>
+            <button className="btn" onClick={handleGetUsers}>Search</button>
           </span>
 
         </div>
       </section>
 
       {/* USER PROFILE */}
-      <section className="user-section">
-        <div className="user-container">
-          <div className="img-container">
-            <img src={users?.avatar_url} alt="" className="user-img" />
-          </div>
-          <h2 className="user-name">{users?.name}</h2>
-          <p className="user-location">{users?.location}</p>
+      
+          {clicked == true ? 
 
-          <div className="follow-div">
-            <span>
-              <p>Followers</p>
-              <p className="counter-follow">{users?.followers}0</p>
-            </span>
-
-            <span className="vertical-line"></span>
-
-            <span>
-              <p>Following</p>
-              <p className="counter-follow">{users?.following}0</p>
-            </span>
-          </div>
-
-        </div>
-      </section>
+          (<section className="user-section">
+            <div className="user-container">
+              <div className="img-container">
+                <img src={users?.avatar_url} alt="" className="user-img" />
+              </div>
+              <h2 className="user-name">{users?.name}</h2>
+              <p className="user-location">{users?.location}</p>
+    
+              <div className="follow-div">
+                <span>
+                  <p>Followers</p>
+                  <p className="counter-follow">{users?.followers}0</p>
+                </span>
+    
+                <span className="vertical-line"></span>
+    
+                <span>
+                  <p>Following</p>
+                  <p className="counter-follow">{users?.following}0</p>
+                </span>
+              </div>
+    
+            </div>
+          </section>) : ("")}
 
     </main>
    );
---
+++ src/components/validation.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export const Validation = () => {
+
+    return (
+        <div className="errorMessage">
+            <p>User Not Found</p>
+        </div>
+    )
+}
